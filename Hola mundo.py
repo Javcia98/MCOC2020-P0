c85db74d72a66058f23e1b0c242150f81772bcdd
Javcia98 <69157429+Javcia98@users.noreply.github.com>
2020-08-03 12:54:39 -0400

    Archivo py

---
+++ Hola mundo.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8
+import scipy as sp
+
+
+nombre = "MCOC"
+dia = 5
+
+print("Forma 1")
+print("hola Jose es el 5 de Agosto")
+
+
+print("Forma 2")
+print("hola " + nombre + " es el " + str(dia) + " de Agosto")
+
+print("Forma 3")
+print("hola {nombre} es el {dia} de Agosto".format(dia=dia, nombre=nombre))
+
+print("Forma 4")
+print(f"hola {nombre} es el {dia} de Agosto")
+
+print("Ejemplo 2 - Forma 1")
+print("hola hola hola hola hola hola hola")
+
+print("Ejemplo 2 - Forma 1")
+print("hola "*3)
+
+
